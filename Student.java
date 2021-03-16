4f9f6767e0d82e6fda2635d914dfbc575af6d133
unknown <miguelpaologs0418@gmail.com>
2021-03-16 15:24:14 +0800

    first commit

---
+++ Student.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class Student {
+	   private double GP;
+	   private int ID;
+	   private int Credits;
+	   private int points;
+	   
+
+	   public Student() {
+	   }
+	   
+	   public int getID() {
+	       return ID;
+	   }
+
+	   public void setID(int aID) {
+	       ID = aID;
+	   }
+
+	   public int getCredits() {
+	       return Credits;
+	   }
+
+	   public void setCredits(int aCredits) {
+	       Credits = aCredits;
+	   }
+
+	   public int getPoints() {
+	       return points;
+	   }
+
+	   public void setPoints(int aPoints) {
+	       points = aPoints;
+	   }
+
+	   public String toString() {
+	       return "ID : " + ID + " Credits : " + Credits + " Points : " + points + " Grade Point : "
+	               + GP;
+	   }
+
+	   public void GPcalculator() {
+	       GP = points / (double) Credits;
+	   }
+	}
